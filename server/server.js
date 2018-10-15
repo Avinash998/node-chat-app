e78b2fc87f6054a7a5aa00f22d5b72f2017bb642
avinash <avinashgolu.chourasia@gmail.com>
2018-10-15 22:34:19 +0530

    newMessage Admin emit

--- server/server.js
+++ server/server.js
@@ -15,6 +15,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage',{
+		from: 'Admin',
+		text: 'Welcome to the chat app',
+		createAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage',{
+		from: 'Admin',
+		text: 'New user Joined',
+		createAt: new Date().getTime()
+	});
+
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
 		io.emit('newMessage',{
@@ -22,6 +34,12 @@ io.on('connection', (socket) => {
 			text: message.text,
 			createAt: new Date().getTime()
 		});
+		/*socket.broadcast.emit('newMessage',{
+			from: message.from,
+			text: message.text,
+			createAt: new Date().getTime()
+		});*/
+
 	});
 
 	socket.on('disconnect', () => {
