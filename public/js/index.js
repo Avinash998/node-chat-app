afedb7eb2e40641eef981220921ba7081ecff2bd
avinash <avinashgolu.chourasia@gmail.com>
2018-10-15 21:45:09 +0530

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Connected to server');
 
-	socket.emit('createMessage',{
-		from: 'Avinash',
-		text: 'Hey. This is Avinash.'
-	});
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -15,14 +15,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
-	socket.emit('newMessage',{
-		from: 'Naman',
-		text: 'See you then',
-		createAt: 234234
-	});
-
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
+		io.emit('newMessage',{
+			from: message.from,
+			text: message.text,
+			createAt: new Date().getTime()
+		});
 	});
 
 	socket.on('disconnect', () => {
