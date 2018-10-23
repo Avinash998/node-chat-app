da8a5ba6f806b172446e723326c58d205bb7e682
avinash <avinashgolu.chourasia@gmail.com>
2018-10-23 22:31:30 +0530

    callback added

--- public/js/index.js
+++ public/js/index.js
@@ -12,3 +12,10 @@ socket.on('disconnect', function () {
 socket.on('newMessage', function (message) {
 	console.log("New Message ", message);
 });
+
+socket.emit('createMessage',{
+	from: 'Frank',
+	text: 'Hello frank'
+},function(data) {
+	console.log(data);
+});
--- server/server.js
+++ server/server.js
@@ -20,8 +20,9 @@ io.on('connection', (socket) => {
 
 	socket.broadcast.emit('newMessage',generateMessage('Admin','New user Joined'));
 
-	socket.on('createMessage', (message) => {
+	socket.on('createMessage', (message, callback) => {
 		io.emit('newMessage',generateMessage(message.from, message.text));
+		callback('This is from the server');
 		/*socket.broadcast.emit('newMessage',{
 			from: message.from,
 			text: message.text,
