59641fc6324196caf6b60721a44896a6f027048f
avinash <avinashgolu.chourasia@gmail.com>
2018-10-24 21:44:48 +0530

    Adding ui for the chat

--- public/js/index.js
+++ public/js/index.js
@@ -11,11 +11,26 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
 	console.log("New Message ", message);
+	var li = jQuery('<li></li>');
+	li.text(`${message.from}: ${message.text}`)
+
+	jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage',{
+/*socket.emit('createMessage',{
 	from: 'Frank',
 	text: 'Hello frank'
 },function(data) {
 	console.log(data);
+});*/
+
+jQuery('#message-form').on('submit', function (e) {
+
+	e.preventDefault();
+	socket.emit('createMessage', {
+		from: 'User',
+		text: jQuery('[name=message]').val()
+	}, function () {
+
+	});
 });
